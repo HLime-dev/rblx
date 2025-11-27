f66816a4a0dac7aa3ac8480a5e482f8883ac78a3
Muhammad Halim <74248915+HLime-dev@users.noreply.github.com>
2025-11-27 09:20:18 +0800

    Update Tes1.lua section

--- Tes1.lua
+++ Tes1.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "DN2",
+   Name = "DN3",
    LoadingTitle = "Dangerous Night",
    LoadingSubtitle = "by Haex",
    ConfigurationSaving = { Enabled = false },
@@ -531,6 +531,7 @@ TeleportTab:CreateButton({
     end
 })
 
+local PlayerSection = Tab:CreateSection("Player")
 -- Player teleport dropdown
 local selectedPlayer = nil
 local function GetPlayerList()
